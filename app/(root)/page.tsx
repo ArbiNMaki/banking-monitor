7659e2be618d45cfa7ed344d9ba496760d7f836f
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-07 03:24:58 +0700

    update: finish create sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -30,7 +30,11 @@ const Home = () => {
         RECENT TRANSACTIONS
       </div>
 
-      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 10000 }, { currentBalance: 9750.85 }]}
+      />
     </section>
   );
 };
